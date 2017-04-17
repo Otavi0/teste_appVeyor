681a103a5400e8d463e9f99ab21add7fcbfd8cac
Otavio Lopes <olopes@mundipagg.com>
2017-04-17 17:41:25 -0300

    :heavy_plus_sign: add deploys automatic

--- TesteAppVeyor/Controllers/ProductsController.cs
+++ TesteAppVeyor/Controllers/ProductsController.cs
@@ -15,7 +15,7 @@ namespace TesteAppVeyor.Controllers
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
             new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
-            new Product { Id = 4, Name = "Teste", Category = "Otavio", Price = 100000.00M }
+            new Product { Id = 4, Name = "Teste1", Category = "OLopes", Price = 100.00M }
         };
 
         public IEnumerable<Product> GetAllProducts()
